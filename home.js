08cd38f1b1902d2bd64dad71747f363d80763ae2
nhu777 <80663254+nhu777@users.noreply.github.com>
2021-05-05 00:52:27 -0700

    Add files via upload

--- home.js
+++ home.js
@@ -4,15 +4,18 @@ function myFunction() {
     var landing = document.getElementById("landing");
     var slack = document.getElementById("slack-case-study");
     var compass = document.getElementById("compass-case-study");
+    var rally = document.getElementById("rally-case-study");
     
     if (slack.style.display === "none") {
         slack.style.display = "block";
         landing.style.display = "none";
         compass.style.display = "none";
+        rally.style.display = "none";
     } else {
         slack.style.display = "none";
         landing.style.display = "block";
         compass.style.display = "none";
+        rally.style.display = "none";
     }
 }
 
@@ -22,15 +25,39 @@ function compass() {
     var landing = document.getElementById("landing");
     var compass = document.getElementById("compass-case-study");
     var slack = document.getElementById("slack-case-study");
+    var rally = document.getElementById("rally-case-study");
     
     if (compass.style.display === "none") {
         compass.style.display = "block";
         landing.style.display = "none";
-        slack.style.display ="none";
+        slack.style.display = "none";
+        rally.style.display = "none";
     } else {
         compass.style.display = "none";
         landing.style.display = "block";
         slack.style.display="none";
+        rally.style.display = "none";
+    }
+}
+
+function rally() {
+    "use strict";
+    
+    var landing = document.getElementById("landing");
+    var compass = document.getElementById("compass-case-study");
+    var slack = document.getElementById("slack-case-study");
+    var rally = document.getElementById("rally-case-study");
+    
+    if (rally.style.display === "none") {
+        compass.style.display = "none";
+        landing.style.display = "none";
+        slack.style.display = "none";
+        rally.style.display = "block";
+    } else {
+        landing.style.display = "block";
+        compass.style.display = "none";
+        slack.style.display="none";
+        rally.style.display = "none";
     }
 }
 
